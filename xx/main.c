10bfc04d9e73abf2c26b3378092e354b8253494b
LEEDUWON <dlendnjs127@gmail.com>
2018-05-24 16:29:22 +0900

    Initial Commit

---
+++ xx/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  xx
+//
+//  Created by LEE on 2018. 5. 24..
+//  Copyright © 2018년 LEE. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
